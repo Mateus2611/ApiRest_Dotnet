119d20d3bd8d31da00aff3e193a66c455ce7eaca
Mateus <MateusHAS.dev@gmail.com>
2024-01-25 14:06:35 -0300

    Adicionando o token JWT

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,5 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
 using TarefasBackEnd.Models;
 using TarefasBackEnd.Models.ViewModels;
 using TarefasBackEnd.Repositories;
@@ -37,8 +41,29 @@ namespace TarefasBackEnd.Controllers
             usuario.Senha = "";
 
             return Ok(new {
-                usuario = usuario
+                usuario = usuario,
+                token = GenerateToken(usuario)
             });
+        }
+
+        private string GenerateToken(Usuario usuario)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler(); 
+
+            var key = Encoding.ASCII.GetBytes("UmTokenMuitoGrandeEDiferenteParaNinguemDescobrir");
+
+            var descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, usuario.Id.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddHours(5),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(descriptor); 
+            return tokenHandler.WriteToken(token);
         }
     }
 }
--- obj/Debug/net8.0/TarefasBackEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/TarefasBackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TarefasBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb406a886e0d2193a493ff5954534e3ef7643db7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab6d932c271afc131e67dae6ea98c83a69e1805")]
 [assembly: System.Reflection.AssemblyProductAttribute("TarefasBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TarefasBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
